a805f67338c9fcb12470a07960209f6e2a330396
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-07-26 13:41:04 +0200

    fix: update manifest

---
+++ android/src/main/java/co/modestnerd/wa_me/WaMe.java
@@ -0,0 +1,179 @@
+package co.modestnerd.wa_me;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
+public class WaMe implements FlutterPlugin, MethodCallHandler {
+    private Context context;
+    private MethodChannel methodChannel;
+
+    @Override
+    public void onAttachedToEngine(FlutterPluginBinding binding) {
+        context = binding.getApplicationContext();
+        methodChannel = new MethodChannel(binding.getBinaryMessenger(), "wa_me");
+        methodChannel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onDetachedFromEngine(FlutterPluginBinding binding) {
+        context = null;
+        methodChannel.setMethodCallHandler(null);
+        methodChannel = null;
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        switch (call.method) {
+            case "shareFile":
+                shareFile(call, result);
+                break;
+            case "share":
+                share(call, result);
+                break;
+            case "isInstalled":
+                isInstalled(call, result);
+                break;
+            default:
+                result.notImplemented();
+        }
+    }
+
+    private boolean isPackageInstalled(String packageName) {
+        try {
+            context.getPackageManager().getPackageInfo(packageName, 0);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("", "Package not found: " + e.getMessage());
+            return false;
+        }
+    }
+
+    private void isInstalled(MethodCall call, Result result) {
+        String packageName = call.argument("package");
+        if (TextUtils.isEmpty(packageName)) {
+            Log.e("", "WaMe: Package name is null or empty");
+            result.error("WaMe: Package name cannot be null or empty", null, null);
+            return;
+        }
+
+        Log.i("", packageName);
+
+        boolean isInstalled = isPackageInstalled(packageName);
+        result.success(isInstalled);
+    }
+
+    private void share(MethodCall call, Result result) {
+        String title = call.argument("title");
+        String text = call.argument("text");
+        String linkUrl = call.argument("linkUrl");
+        String chooserTitle = call.argument("chooserTitle");
+        String phone = call.argument("phone");
+        String packageName = call.argument("package");
+
+        if (TextUtils.isEmpty(title)) {
+            Log.e("", "WaMe: Title is null or empty");
+            result.error("WaMe: Title cannot be null or empty", null, null);
+            return;
+        } else if (TextUtils.isEmpty(phone)) {
+            Log.e("", "WaMe: Phone is null or empty");
+            result.error("WaMe: Phone cannot be null or empty", null, null);
+            return;
+        } else if (TextUtils.isEmpty(packageName)) {
+            Log.e("", "WaMe: Package name is null or empty");
+            result.error("WaMe: Package name cannot be null or empty", null, null);
+            return;
+        }
+
+        ArrayList<String> extraTextList = new ArrayList<>();
+
+        if (!TextUtils.isEmpty(text)) {
+            extraTextList.add(text);
+        }
+        if (!TextUtils.isEmpty(linkUrl)) {
+            extraTextList.add(linkUrl);
+        }
+
+        String extraText = TextUtils.join("\n\n", extraTextList);
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.setPackage(packageName);
+        intent.putExtra("jid", phone + "@s.whatsapp.net");
+        intent.putExtra(Intent.EXTRA_SUBJECT, title);
+        intent.putExtra(Intent.EXTRA_TEXT, extraText);
+
+        try {
+            context.startActivity(intent);
+            result.success(true);
+        } catch (Exception ex) {
+            Log.e("", "WaMe: Error sharing message");
+            result.error("WaMe: Error sharing message", null, null);
+        }
+    }
+
+    private void shareFile(MethodCall call, Result result) {
+        String title = call.argument("title");
+        String text = call.argument("text");
+        ArrayList<String> filePaths = call.argument("filePath");
+        String chooserTitle = call.argument("chooserTitle");
+        String phone = call.argument("phone");
+        String packageName = call.argument("package");
+
+        if (filePaths == null || filePaths.isEmpty()) {
+            Log.e("", "WaMe: ShareLocalFile Error: filePath is null or empty");
+            result.error("WaMe: FilePath cannot be null or empty", null, null);
+            return;
+        } else if (TextUtils.isEmpty(phone)) {
+            Log.e("", "WaMe: Phone is null or empty");
+            result.error("WaMe: Phone cannot be null or empty", null, null);
+            return;
+        } else if (TextUtils.isEmpty(packageName)) {
+            Log.e("", "WaMe: Package name is null or empty");
+            result.error("WaMe: Package name cannot be null or empty", null, null);
+            return;
+        }
+
+        ArrayList<Uri> files = new ArrayList<>();
+
+        for (String filePath : filePaths) {
+            File file = new File(filePath);
+            Uri fileUri = FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".provider", file);
+            files.add(fileUri);
+        }
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND_MULTIPLE);
+        intent.setType("*/*");
+        intent.setPackage(packageName);
+        intent.putExtra("jid", phone + "@s.whatsapp.net");
+        intent.putExtra(Intent.EXTRA_SUBJECT, title);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, files);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        try {
+            context.startActivity(intent);
+            result.success(true);
+        } catch (Exception ex) {
+            Log.e("", "WaMe: Error sharing file");
+            result.error("WaMe: Error sharing file", null, null);
+        }
+    }
+}
